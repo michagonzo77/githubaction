9a31aaba852c68aebb03f3670685c16e20f723d0
michagonzo77 <michagonzo77@gmail.com>
2024-02-26 17:56:54 +0000

    Appended lines to pulumi.ts

--- pulumi.ts
+++ pulumi.ts
@@ -2,3 +2,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { EventEmitter } from 'events';
 import { Readable } from 'stream';
+nconst config = new pulumi.Config();
+const cider = config.require(cider);
+const stackName = config.require(stackName);
