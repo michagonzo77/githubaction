ab68e6dd29470e18ceca9a34324782c8059e51de
michagonzo77 <michagonzo77@gmail.com>
2024-02-26 17:52:20 +0000

    Appended lines to pulumi.ts

--- pulumi.ts
+++ pulumi.ts
@@ -7,3 +7,8 @@ echo ##active_line2##
 const cider = config.require('cider');
 echo ##active_line3##
 const stackName = config.require('stackName');
+const config = new pulumi.Config();
+echo ##active_line2##
+const cider = config.require('cider');
+echo ##active_line3##
+const stackName = config.require('stackName');
